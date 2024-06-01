5b8a4747bf15b821838f77e08bf23853148e4ee6
Cristian Vianey Cáceres Labrador <80643642+cristiancacereslabrador@users.noreply.github.com>
2024-06-01 17:59:52 -0500

    changePriceToId

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,7 +3,8 @@ import Product from "../models/Product.model";
 
 export const getProducts = async (req: Request, res: Response) => {
   const products = await Product.findAll({
-    order: [["price", "DESC"]],
+    // order: [["price", "DESC"]],//MOD
+    order: [["id", "DESC"]],
     // attributes: { exclude: ["createdAt", "updatedAt"] },
   });
   res.json({ data: products });
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import db from "./config/db";
 import colors from "colors";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 
 //**CONECTAR A BASE DE DATOS */
 export async function connectDB() {
@@ -23,8 +25,24 @@ connectDB();
 
 //**INSTANCIA DE EXPRESS */
 const server = express();
+//**PERMITIR CONEXIONES (CORS) */
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    // console.log("origin", origin);
+    if (origin === process.env.FRONTEND_URL) {
+      //   console.log("permitir");
+      callback(null, true);
+    } else {
+      //   console.log("denegar");
+      callback(new Error("Error de CORS"));
+    }
+  },
+};
+server.use(cors(corsOptions));
 //**LEER DATOS DE FORMULARIOS */
 server.use(express.json());
+
+server.use(morgan("dev"));
 //**ROUTING */
 server.use("/api/products", router);
 //**ELIMINADO PARA LA DOC API */
